7f23249197ad11115d471499f5ebf6344b4b96ee
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-22 22:52:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const formData = {
+    name: "Byron",
+    email: "Poodle@gmail.com",
+    UserId: 1
+};
+
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/users", submitData)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
